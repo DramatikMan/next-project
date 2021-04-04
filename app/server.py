38cd4c90103042dc90a8629bfdf3ce1061571708
Sergey Pavlov <self@sergeypavlov.dev>
2021-04-04 22:29:24 +0300

    Try Flask-like route declaring

--- app/server.py
+++ app/server.py
@@ -1,17 +1,18 @@
 from aiohttp import web
 
 
-async def handle(request):
-    name = request.match_info.get('name', 'Anonymous')
-    text = 'Hello, ' + name
+routes = web.RouteTableDef()
+
+
+@routes.get('/')
+async def hello(request):
+    name = request.match_info.get('name', "Anonymous")
+    text = "Hello, " + name
     return web.Response(text=text)
 
 
 app = web.Application()
-app.add_routes([
-    web.get('/', handle),
-    web.get('/{name}', handle)
-])
+app.add_routes(routes)
 
 
 if __name__ == '__main__':
