d78b1c2cb7cd0ebc1195e3a1b75a24ce62289efc
Sergey Pavlov <self@sergeypavlov.dev>
2021-04-10 18:19:55 +0300

    Switch to no daemon mode

--- scripts/supervisord.sh
+++ scripts/supervisord.sh
@@ -1,18 +1,16 @@
 pip install supervisor
-touch /tmp/supervisord.log
 echo_supervisord_conf > /etc/supervisord.conf
 
 echo $'
 [program:aiohttp]
-user=root
+directory=/project
 numprocs=4
 numprocs_start=1
 process_name=aiohttp_%(process_num)s
-command=pipenv run python app.py --path=/tmp/aiohttp_%(process_num)s.sock
+command=pipenv run python app.py --path=/tmp/aiohttp/%(process_num)s.sock
 autostart=true
 autorestart=true' >> /etc/supervisord.conf
 
 supervisord \
 --configuration=/etc/supervisord.conf \
---directory=/project \
---logfile=/project/supervisord.log
+--nodaemon
