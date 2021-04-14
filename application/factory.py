99c6d3bf9abe15ec234130690096dc243dbf0dae
Sergey Pavlov <self@sergeypavlov.dev>
2021-04-14 18:52:11 +0300

    Add template rendering for base route

--- application/factory.py
+++ application/factory.py
@@ -1,17 +1,22 @@
 from aiohttp import web
+import jinja2
+import aiohttp_jinja2
 
 
 routes = web.RouteTableDef()
+loader = jinja2.FileSystemLoader('/project/application/templates')
 
 
 @routes.get('/')
+@aiohttp_jinja2.template('index.html')
 async def hello(request):
     name = request.match_info.get('name', "Anonymous")
-    text = "Hello, " + name
-    return web.Response(text=text)
+    text = 'Hello, ' + name
+    return {'text': text}
 
 
 def create_app():
     app = web.Application()
     app.add_routes(routes)
+    aiohttp_jinja2.setup(app, loader=loader)
     return app
